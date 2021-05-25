8e03b47632b945e47ba20f3a72700f5798b44675
Your Name <911524028@qq.com>
2021-05-25 19:21:58 +0800

    second commit...

--- stockSystem/stockSystem/urls.py
+++ stockSystem/stockSystem/urls.py
@@ -20,8 +20,6 @@ from stockUser import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^index/',views.index),
     url(r'^login/',views.login),
-    url(r'^register/',views.register),
-    url(r'^logout/',views.logout)
+    url(r'^captcha',include('captcha.urls'))
 ]
